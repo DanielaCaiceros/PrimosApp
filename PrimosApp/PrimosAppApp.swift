ef29fe5e6013746296365312a958dc95cffa44da
Daniela Caiceros <caiceros.flores.daniela@gmail.com>
2024-10-12 16:32:46 -0600

    initial commit

---
+++ PrimosApp/PrimosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimosAppApp.swift
+//  PrimosApp
+//
+//  Created by Daniela Caiceros on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PrimosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
